e32bf764309ea727db2e47635cbc30137519b7a5
Affan Najiy <affannajiy@gmail.com>
2025-11-06 18:26:08 +0800

    Update

--- src/server.js
+++ src/server.js
@@ -4,7 +4,13 @@ const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const pool = new Pool({ /* same */ });
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 app.use(express.static('public'));
 
